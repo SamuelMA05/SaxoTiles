ecdda86ee88172dd4de7e41df83674faaf6ebe15
Nicolás H <ndhg2004@gmail.com>
2026-05-24 20:28:47 -0500

    Delete T113_SAXO_OS directory

--- T113_SAXO_OS/debian/build_debian.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./debian_first_stage.sh
-./debian_second_stage.sh
-./debian_saxo_stage.sh
-
--- T113_SAXO_OS/debian/debian_umount.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-TARGET_DIR=debian_fs
-
-for fs in $(mount | grep "debian_fs"  | awk '{print $3}' | awk '{print length, $0}' | sort -nr | cut -d' ' -f2-); do
-   echo umounting $fs
-   sudo umount $fs
-done
